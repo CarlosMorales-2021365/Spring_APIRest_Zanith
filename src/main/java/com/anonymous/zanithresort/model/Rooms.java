6de1af52e69876b49d0a6a0342b5ba627ea7b9d4
Carlos <moralesc21365@gmail.com>
2024-09-04 22:00:57 -0600

    feat(model):
    se creo el paquete model y la clase rooms

---
+++ src/main/java/com/anonymous/zanithresort/model/Rooms.java
@@ -0,0 +1,32 @@
+package com.anonymous.zanithresort.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="rooms")
+public class Rooms implements Serializable {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long idRoom;
+    private String roomType;
+    private double cost;
+    private String capacity;
+    private String availability;
+    private String description;
+    private Date starDate;
+    private Date endDate;
+
+
+    // @OneToMany
+    // Private Hotel hotel;
+    
+}
