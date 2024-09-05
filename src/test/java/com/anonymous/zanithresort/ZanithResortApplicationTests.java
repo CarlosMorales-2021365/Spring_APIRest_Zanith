c47115d01a26b049c9a770400a4d7ea210e06469
jose moises Ramírez gaitan <jramirez-2023013@kinal.edu.gt>
2024-09-04 21:07:50 -0600

    build(ft/joseph_2023013):se agrego el proyecto

---
+++ src/test/java/com/anonymous/zanithresort/ZanithResortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonymous.zanithresort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZanithResortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
