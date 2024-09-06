11d4ccaff2acb6d304119a8bb364104daa52a48a
jose moises Ramírez gaitan <jramirez-2023013@kinal.edu.gt>
2024-09-05 19:28:53 -0600

    feat(Model,DTOs,Utils):package model y clase User,Package utils y clase enum Status,Package DTOs y clase userRegisterDTO

---
+++ src/main/java/com/anonymous/zanithresort/models/User.java
@@ -0,0 +1,52 @@
+package com.anonymous.zanithresort.models;
+
+import java.io.Serializable;
+
+import com.anonymous.zanithresort.DTOs.UserRegisterDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "User")
+public class User implements Serializable {
+    @Id
+    @Column(name = "id")
+    private long id;
+
+    @Email(message = "Ingresa Direccion de correo Valida")
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String username;
+
+    private String name;
+    @Column(unique = true)
+    private String surname;
+
+    @Column(unique = true)
+    private String password;
+
+    @Column(unique = true)
+    private String phone;
+
+    private String role;
+    private String urlProfilePhoto;
+
+    public User(UserRegisterDTO userDTO, String img) {
+        this.email = userDTO.getEmail();
+        this.username = userDTO.getUsername();
+        this.password = userDTO.getPassword();
+        this.name = userDTO.getName();
+        this.surname = userDTO.getSurname();
+        this.role = userDTO.getRole();
+        this.phone = userDTO.getPhone();
+        this.urlProfilePhoto = img;
+    }
+}
