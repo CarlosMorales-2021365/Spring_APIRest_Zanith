802698978a40da5d3f1010f5f09d7d51aff75ec1
Fredy <alexander.garcia.sicajau@gmail.com>
2024-09-10 09:31:28 -0600

    feat(ServiceReservation): Se realizo el servicio de reservacion

--- src/main/java/com/anonymous/zanithresort/service/IReservationService.java
+++ src/main/java/com/anonymous/zanithresort/service/IReservationService.java
@@ -1,5 +1,13 @@
 package com.anonymous.zanithresort.service;
+import java.util.List
 
-public class IReservationService {
-    
+public interface IReservationService {
+
+    public List <Reservation> listReservation();
+
+    public Reservation finReservation (Integer ID_Reservation);
+
+    public Reservation saveReservation (Reservation reservation);
+
+    public void deleteReservation (Reservation reservation);
 }
