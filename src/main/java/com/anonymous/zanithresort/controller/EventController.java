8adf0f5ea8d6663ec9ea4902d4bc8f1f0a6fc2f9
Bryanth87 <Brayan TG>
2024-09-13 19:00:48 -0600

    feat: (ft/brayan_2023014) creación de método FindEvent en la clase EventController

--- src/main/java/com/anonymous/zanithresort/controller/EventController.java
+++ src/main/java/com/anonymous/zanithresort/controller/EventController.java
@@ -6,10 +6,13 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anonymous.zanithresort.exception.EventException;
 import com.anonymous.zanithresort.model.Event;
 import com.anonymous.zanithresort.services.IEventService;
 
@@ -28,6 +31,14 @@ public class EventController {
         event2.forEach(events -> logger.info(events.toString()));
         return event2;
 
+    }
+
+    @GetMapping("/FindEvents/{id_Event}")
+    public ResponseEntity<Event> findEvent(@PathVariable Integer id_Event) {
+        Event events = iEventService.findEvent(id_Event);
+        if (id_Event == null)
+            throw new EventException("No se encuentra el evento");
+        return ResponseEntity.ok(events);
     }
 }
 
