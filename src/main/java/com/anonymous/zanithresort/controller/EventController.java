03390c08fec2ba20484ea543e1528e82acbb2673
Bryanth87 <Brayan TG>
2024-09-13 19:03:05 -0600

    feat: (ft/brayan_2023014) creación de método AddEvents en la clase EventController

--- src/main/java/com/anonymous/zanithresort/controller/EventController.java
+++ src/main/java/com/anonymous/zanithresort/controller/EventController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +41,13 @@ public class EventController {
         if (id_Event == null)
             throw new EventException("No se encuentra el evento");
         return ResponseEntity.ok(events);
+        
+    }
+
+    @PostMapping("/AddEvents") 
+    public Event addEvents(@RequestBody Event event){
+        logger.info("Evento agregado" + event);
+        return iEventService.addEvent(event);
     }
 }
 
