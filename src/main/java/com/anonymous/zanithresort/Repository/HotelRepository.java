2b2806997dbb7299dbd2c0e4a33f05c1795c6c1e
Carlos <moralesc21365@gmail.com>
2024-09-24 11:17:27 -0600

    feat(repository):eliminacion de la clase

--- src/main/java/com/anonymous/zanithresort/Repository/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anonymous.zanithresort.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.anonymous.zanithresort.model.Hotels;
-
-public interface HotelRepository  extends JpaRepository<Hotels, Integer> {
-
-}
