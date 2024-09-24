f794df0cacdf040720091198a10b1e4021b746f6
Carlos <moralesc21365@gmail.com>
2024-09-24 11:28:11 -0600

    feat(correccion)  correccion de clase

--- src/main/java/com/anonymous/zanithresort/config/CloudinaryConfig.java
+++ src/main/java/com/anonymous/zanithresort/config/CloudinaryConfig.java
@@ -12,16 +12,10 @@ public class CloudinaryConfig {
     @Bean
     Cloudinary cloudinary(){
         return new Cloudinary(ObjectUtils.asMap(
-<<<<<<< HEAD
-            "cloud_name","dcrgnm3ud",
-            "api_key", "655453188429764",
-            "api_secret", "7DJJn3dB1hGQNAYYY0xM81Xr05M"
-=======
 
                 "cloud_name","djlfcyqqb",
                 "api_key","931248142449731",
                 "api_secret","ALBbFvAbNnhW6a7cStRbpQaJXhU"
->>>>>>> b4bb9312c9fe8314d562c8d67feee6225a5bd33b
         ));
     }
 }
