ae9843bbc709fb8250c0904d6c9e92ec0a453e8b
Carlos <moralesc21365@gmail.com>
2024-09-24 09:04:15 -0600

    Feat(repository):se elimino paquete

--- src/main/java/com/anonymous/zanithresort/Repository/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anonymous.zanithresort.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.anonymous.zanithresort.model.Reservation;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Integer>{
-
-}
